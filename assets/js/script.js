eac4f25fc3cba8c9f27a122de4937d552031151c
dylan fair <dylanfair11@gmail.com>
2021-09-13 21:04:55 -0500

    add data persistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,8 +140,6 @@ var taskButtonHandler = function(event) {
 };
 
 var taskStatusChangeHandler = function(event) {
-  console.log(event.target.value);
-
   // find task list item based on event.target's data-task-id attribute
   var taskId = event.target.getAttribute("data-task-id");
 
@@ -206,6 +204,41 @@ var deleteTask = function(taskId) {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+var loadTasks = function() {
+  tasks = localStorage.getItem("tasks", tasks);
+  if (tasks === null) {
+    tasks = [];
+    return false;
+  }
+  tasks = JSON.parse(tasks);
+  for (var i =0; i < tasks.length; i++) {
+    tasks[i].id = taskIdCounter;
+    console.log(tasks[i]);
+    var listItemEl = document.createElement("li")
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'" + tasks[i].type +"</span>";
+    listItemEl.appendChild(taskInfoEl);
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+    if (tasks[i].status === "to do") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+    }
+    else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl);
+    }
+    else if (tasks[i].status === "complete") {
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+      tasksCompletedEl.appendChild(listItemEl);
+    }
+    taskIdCounter++;
+  }
+  console.log(listItemEl);
 };
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
